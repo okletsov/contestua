dae8dee6370f610b171dd118c74f89197709facb
akletsov88 <akletsov88@gmail.com>
2024-06-03 02:08:45 +0000

    reverting all the way back

--- pages/Рейтинг/jsobjects/utils/utils.js
+++ pages/Рейтинг/jsobjects/utils/utils.js
@@ -1,7 +1,6 @@
 export default {
 	customWidgetData() {
 		return {
-			"contestLabels": contest_labels.data,
 			"liveRating": rating_live.data,
 			"ratingStatic": rating_static.data,
 			"crRawOngoing": cr_raw_ongoing.data	
