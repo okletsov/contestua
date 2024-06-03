ba10b90834f97697f090281c1882a084de7658b5
akletsov88 <akletsov88@gmail.com>
2024-06-03 01:25:06 +0000

    reverting due to race condition

--- pages/Рейтинг/jsobjects/utils/utils.js
+++ pages/Рейтинг/jsobjects/utils/utils.js
@@ -1,10 +1,10 @@
 export default {
 	customWidgetData() {
 		return {
+			"contestLabels": contest_labels.data,
 			"liveRating": rating_live.data,
 			"ratingStatic": rating_static.data,
-			"crRawOngoing": cr_raw_ongoing.data,
-			"contestLabels": contest_labels.data
+			"crRawOngoing": cr_raw_ongoing.data	
 		}
 	}
 }
