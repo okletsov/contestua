e4eecc3d56a8ff62cd8a0d890ef2a774941ca8fe
akletsov88 <akletsov88@gmail.com>
2025-01-28 21:39:13 +0000

    live standings for annual contest winner

--- pages/Победитель года/jsobjects/utils/utils.js
+++ pages/Победитель года/jsobjects/utils/utils.js
@@ -1,8 +1,13 @@
 export default {
-	customWidgetData() {
+	priorContestsData() {
 		return {
 			"crAnnual": cr_annual.data,
 			"crAnnualFinances": cr_annual_finances.data
+		}
+	},
+	liveContestData() {
+		return {
+			"crAnnualLive": cr_annual_live.data
 		}
 	}
 }
